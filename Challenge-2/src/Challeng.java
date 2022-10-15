0fecda6078912bf14daa92016e3f59edb5d45c13
Mayu Hayakawa <mayu.i.19930327@gmail>
2022-10-14 17:21:06 -0700

    made short code

--- Challenge-2/src/Challeng.java
+++ Challenge-2/src/Challeng.java
@@ -2,22 +2,17 @@ public class Challeng {
 
     public static void main(String[] args) throws Exception {
 
-        //make instances
-        Fiction fiction = new Fiction("Harry Potter");
-        NonFiction nonFiction = new NonFiction("Calculus");
-
         //make array(bookList) of fiction and nonfiction classes's instance
         Book[] bookList = new Book[2];
 
-        //store references to array
-        bookList[0] = fiction;
-        bookList[1] = nonFiction;
-
-        System.out.println("Title of fiction book:");
-        System.out.println(bookList[0].getTitle());
-        System.out.println("Title of non fiction book:");
-        System.out.println(bookList[1].getTitle());
-        System.out.println("Title-" + bookList[0].getTitle() + " Cost-$" + bookList[0].getPrice());
-        System.out.println("Title-" + bookList[1].getTitle() + " Cost-$" + bookList[1].getPrice());
+        //make instances and store references to array
+        bookList[0] = new Fiction("Harry Potter");
+        bookList[1] = new NonFiction("Calculus");
+        
+        System.out.println("Title of fiction book:\n" + bookList[0].getTitle());
+        System.out.println("Title of non fiction book:\n" + bookList[1].getTitle());
+        for(int i = 0; i < bookList.length; i++) {
+            System.out.println("Title-" + bookList[i].getTitle() + " Cost-$" + bookList[i].getPrice());
+        }
     }
 }
