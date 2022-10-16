146b85920debd183b04e48c46c4e03a430392612
Maho Ando <111376661+MahoAndo@users.noreply.github.com>
2022-10-16 01:13:22 -0700

    Update Book.java

--- Challenge-2/src/Book.java
+++ Challenge-2/src/Book.java
@@ -1,28 +1,28 @@
 public abstract class Book {
-	public String title;
-	public double price;
+    public String title;
+    public double price;
 
-	public Book() {
+    public Book() {
 
-	}
+    }
 
-	public Book(String title, double price) {
+    public Book(String title, double price) {
 
-		this.title = title;
-		this.price = price;
-	}
+	this.title = title;
+	this.price = price;
+    }
 
-	//getter
-	public String getTile() {
+    //getter
+    public String getTile() {
 
 	return this.title;
-	}
+    }
 
-	//getter
-	public double getPrice() {
+    //getter
+    public double getPrice() {
 
 	return this.price;
-	}
+    }
 
-	public abstract double setPrice();
+    public abstract double setPrice();
 }
