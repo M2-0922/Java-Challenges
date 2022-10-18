920066f1bdc69c5d4c1a456e138ad2f07b5817d5
Kubilay Cakmak <kubilayckmk@gmail.com>
2022-10-17 20:22:17 -0700

    Challenge 2 complete v1

--- Challenge-2/src/App.java
+++ Challenge-2/src/App.java
@@ -2,11 +2,18 @@ public class App {
     public static void main(String[] args) {
         // write your code here
         Book[] books = new Book[2];
-        books[0] = new Fiction("The Great Gatsby");
-        books[1] = new NonFiction("The Great Gatsby");
+        books[0] = new Fiction("Harry Potter");
+        books[1] = new NonFiction("Calculus");
+
+        // System.out.println("Title of fiction book:");
+        // System.out.println(books[0].getTitle());
+        // System.out.println("Title of non fiction book:");
+        // System.out.println(books[1].getTitle());
 
         for (Book book : books) {
-            System.out.println(book.getTitle() + " costs $" + book.getPrice());
+            System.out.println("Title of " + book.getClass().getSimpleName() + " book:");
+            System.out.println(book.getTitle());
+            System.out.println(book.getTitle() + " Cost-$" + book.getPrice());
         }
     }
 }
