ec52b1df71fe278a259547b4c8b112921551e866
Aya <pyon220@gmail.com>
2022-10-18 10:10:34 -0700

    added another answer

--- Challenge-2/src/App.java
+++ Challenge-2/src/App.java
@@ -13,12 +13,19 @@ public class App {
         // for(int i=0;i<book.length;i++){
         //     book[i].showInfo();
         // }
-        System.out.println("====================================");
+        System.out.println("=============== way1 =====================");
         for(int i = 0; i < book.length; i++){
             System.out.println("Title of " + (i == 0 ? "fiction" : "non fiction") + " book: " + "\n" + book[i].getBookTitle());
         }
         for(int i = 0; i < book.length; i++){
             System.out.println("Title-" + book[i].getBookTitle() + " " + "Cost-" + "$" +book[i].getBookPrice());
+        }
+        System.out.println("================ way2 ====================");
+        // For loop in Array (Name of Class/name/name of array)
+        for(Book books:book){
+            System.out.println("Title of "+books.getClass().getSimpleName()+ " book.");
+            System.out.println(books.getBookTitle());
+            System.out.println(books.getBookTitle() + " Cost-$" + books.getBookPrice());
         }
     }
 }
